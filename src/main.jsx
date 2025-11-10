1122c2a75b0e4b707cdfb1b4d8d044774523b249
Syriana G <sgil0074@launchpadphilly.org>
2025-11-10 11:17:03 -0500

    fixed APP.JSX

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-<<<<<<< HEAD
 import { BrowserRouter } from 'react-router-dom'
-=======
-import { BrowserRouter } from 'react-router-dom';
->>>>>>> 3e13479b94971f9f44624de982c1434f8ee8c517
 import './index.css';
 import App from './App';
 
